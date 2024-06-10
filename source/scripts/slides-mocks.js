512cc61cb9dae3e49f742bedca0f30c1cf4148d4
Randell87 <randell@mail.ru>
2024-06-10 19:11:28 +0700

    Создаём моковые данные для слайдов

---
+++ source/scripts/slides-mocks.js
@@ -0,0 +1,71 @@
+export const slides = [
+  {
+    title: 'Декаф Флэт Уайт',
+    image:
+      `<picture>
+    <source type="image/webp" media="(min-width: 1440px)"
+      srcset="images/hero-1-desktop@1x.webp 1x, images/hero-1-desktop@2x.webp 2x" width="600" height="600">
+    <source type="image/webp" media="(min-width: 768px)"
+      srcset="images/hero-1-tablet@1x.webp 1x, images/hero-1-tablet@2x.webp 2x" width="476" height="476">
+    <source type="image/webp" srcset="images/hero-1-mobile@1x.webp 1x, images/hero-1-mobile@2x.webp 2x"
+      width="280" height="280">
+    <source media="(min-width: 1440px)"
+      srcset="images/hero-1-desktop@1x.png 1x, images/hero-1-desktop@2x.png 2x" width="600" height="600">
+    <source media="(min-width: 768px)" srcset="images/hero-1-tablet@1x.png 1x, images/hero-1-tablet@2x.png 2x"
+      width="476" height="476">
+    <img class="slider__image" src="images/hero-1-mobile@1x.png" srcset="images/hero-1-mobile@2x.png 2x"
+      alt="" width="280" height="280">
+  </picture>`
+    ,
+    description: 'Свежесваренный кофе без кофеина из Эфиопии с&nbsp;натуральным фермерским молоком — то, что нужно для&nbsp;расслабления после тяжёлого рабочего дня',
+    oldPrice: '295₽',
+    price: '225₽',
+    color: '#f3ebe1',
+  },
+  {
+    title: 'Лавандовый Латте',
+    image:
+      `<picture>
+  <source type="image/webp" media="(min-width: 1440px)"
+    srcset="images/hero-2-desktop@1x.webp 1x, images/hero-2-desktop@2x.webp 2x" width="600" height="600">
+  <source type="image/webp" media="(min-width: 768px)"
+    srcset="images/hero-2-tablet@1x.webp 1x, images/hero-2-tablet@2x.webp 2x" width="476" height="476">
+  <source type="image/webp" srcset="images/hero-2-mobile@1x.webp 1x, images/hero-2-mobile@2x.webp 2x"
+    width="280" height="280">
+  <source media="(min-width: 1440px)"
+    srcset="images/hero-2-desktop@1x.png 1x, images/hero-2-desktop@2x.png 2x" width="600" height="600">
+  <source media="(min-width: 768px)" srcset="images/hero-2-tablet@1x.png 1x, images/hero-2-tablet@2x.png 2x"
+    width="476" height="476">
+  <img class="slider__image" src="images/hero-2-mobile@1x.png" srcset="images/hero-2-mobile@2x.png 2x"
+    alt="" width="280" height="280">
+</picture>`
+    ,
+    description: 'Невероятное сочетание перуанской высокогорной арабики с молоком ламы и лавандовым сиропом унесёт вас прямо на вершину Радужных гор',
+    oldPrice: '285₽',
+    price: '265₽',
+    color: '#eae6fc',
+  },
+  {
+    title: 'Тройной Эспрессо',
+    image:
+      `<picture>
+  <source type="image/webp" media="(min-width: 1440px)"
+    srcset="images/hero-3-desktop@1x.webp 1x, images/hero-3-desktop@2x.webp 2x" width="600" height="600">
+  <source type="image/webp" media="(min-width: 768px)"
+    srcset="images/hero-3-tablet@1x.webp 1x, images/hero-3-tablet@2x.webp 2x" width="476" height="476">
+  <source type="image/webp" srcset="images/hero-3-mobile@1x.webp 1x, images/hero-3-mobile@2x.webp 2x"
+    width="280" height="280">
+  <source media="(min-width: 1440px)"
+    srcset="images/hero-3-desktop@1x.png 1x, images/hero-3-desktop@2x.png 2x" width="600" height="600">
+  <source media="(min-width: 768px)" srcset="images/hero-3-tablet@1x.png 1x, images/hero-3-tablet@2x.png 2x"
+    width="476" height="476">
+  <img class="slider__image" src="images/hero-3-mobile@1x.png" srcset="images/hero-3-mobile@2x.png 2x"
+    alt="" width="280" height="280">
+</picture>`
+    ,
+    description: 'Мощнее укола адреналина, чернее самой тёмной ночи, этот тройной эспрессо из Колумбии покажет вам, что такое настоящая бодрость',
+    oldPrice: '395₽',
+    price: '375₽',
+    color: '#e5e6e8',
+  },
+];
